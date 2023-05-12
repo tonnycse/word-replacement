7f0e0309e8b4449fbd8fcec485e346bedab73993
tonnycse <tonnyalam98@gmail.com>
2023-05-12 11:11:07 -0700

    first commit

---
+++ wordreplacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+
+    str = "Hi guys, I am Tonny."
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+
+replace_word() 
